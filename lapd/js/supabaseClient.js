5e41530ab8296b85aa7234e56ee73c11bdd9c916
Taspooooooooooooooooooooo <126410408+Taspooooooooooooooooooooo@users.noreply.github.com>
2026-07-03 22:10:14 +0200

    Refactor supabase client initialization

--- lapd/js/supabaseClient.js
+++ lapd/js/supabaseClient.js
@@ -1,11 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const SUPABASE_URL = "https://vtqyqzuhifzqzqszhtwq.supabase.co";
-const SUPABASE_KEY = "sb_publishable_NunfAEMxNJA39nzFxtn42g_hsmzxcv8";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-window.supabase = supabase.createClient(
+window.supabase = window.supabase || supabase.createClient(
   "https://vtqyqzuhifzqzqszhtwq.supabase.co",
   "sb_publishable_NunfAEMxNJA39nzFxtn42g_hsmzxcv8"
 );
