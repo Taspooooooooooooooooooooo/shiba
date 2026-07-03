a552297ae72f5758e0111c476177fc21f2fc9eeb
Taspooooooooooooooooooooo <126410408+Taspooooooooooooooooooooo@users.noreply.github.com>
2026-07-03 21:49:56 +0200

    Update supabaseClient.js

--- lapd/js/supabaseClient.js
+++ lapd/js/supabaseClient.js
@@ -4,3 +4,8 @@ const SUPABASE_URL = "https://vtqyqzuhifzqzqszhtwq.supabase.co";
 const SUPABASE_KEY = "sb_publishable_NunfAEMxNJA39nzFxtn42g_hsmzxcv8";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+window.supabase = supabase.createClient(
+  "https://vtqyqzuhifzqzqszhtwq.supabase.co",
+  "sb_publishable_NunfAEMxNJA39nzFxtn42g_hsmzxcv8"
+);
